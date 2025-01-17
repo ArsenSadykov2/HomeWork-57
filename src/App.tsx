c86919f9514e2c84592e95c8cf1b64e2a8b7f95f
Sadykov Arsen <LoCo10291998@gmail.com>
2025-01-17 18:05:15 +0600

    Second Commit. We created a folder for interfaces and created an array for users

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 
 import './App.css'
+import {useState} from "react";
+import {User} from "./types.s.ts";
 
-const App = () => (
-    <>
+const App = () => {
+    const [users, setUsers] = useState<User[]>([]);
 
-    </>
-);
+
+    return (
+        <div className="container mt-4">
+            <div className="row">
+                <div className="col-md-6">
+                    <h3>Создание пользователя</h3>
+                    <UserForm />
+                </div>
+                <div className="col-md-6">
+                    <h3>Список пользователей</h3>
+                    <Users users={users}/>
+                </div>
+            </div>
+        </div>
+    )
+};
+
 
 export default App
